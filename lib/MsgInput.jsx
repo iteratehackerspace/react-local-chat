163564f177a18e85878f178263df4243698e2817
Edgar Khanzadian <edgar.khanzadian@gmail.com>
2016-10-13 21:09:48 +0400

    Add MsgInput, chat

--- lib/MsgInput.jsx
+++ lib/MsgInput.jsx
@@ -10,19 +10,22 @@ class MsgInput extends React.Component {
     this.click_handler = this.click_handler.bind(this);
     this.form_changed = this.form_changed.bind(this);
     this.state = {msg : ''};
+
   }
-  
+
    async click_handler(event) {
     let req_opts = {
       method:'post',
-      mode: 'no-cors',
-      headers: {
+      headers: new Headers({
         'Accept': 'application/json',
         'Content-Type': 'application/json'
-      },
+      }),
       body:JSON.stringify({'msg':this.state.msg})
     };
-    await fetch('http://localhost:8000/message', req_opts);
+    await fetch('http://192.168.1.216:8000/message', req_opts);
+    this.props.updater(this.state.msg);
+    this.setState({msg: ''});
+
   }
 
   form_changed (e) {
@@ -38,7 +41,7 @@ class MsgInput extends React.Component {
       textAlign: 'center',
       textDecoration: 'none',
       display: 'inline-block',
-      fontSize: '20'}
+      fontSize: '20px'}
 
     let msgDivStyle = {
       position: "fixed",
@@ -51,7 +54,7 @@ class MsgInput extends React.Component {
         <input type="text"
                 onChange={this.form_changed}
                 value={this.state.msg}/>
-        <button style = {msgButtonStyle} onClick = {this.click_handler}>
+        <button style = {msgButtonStyle} onClick = {this.click_handler} >
 	        Send
         </button>
       </div>
--- lib/chat.jsx
+++ lib/chat.jsx
@@ -4,15 +4,43 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import MsgInput from './MsgInput';
 import StatusBar from './statusBar';
+import ChatHistory from './chathistory';
+
 class ChatApp extends React.Component {
+
+  constructor() {
+    super();
+    this.state = {msgs : []};
+  }
+
+  componentDidMount() {
+
+    setInterval(async () => {
+      console.log('Called');
+  
+      let messageHistory = await fetch('http://192.168.1.216:8000/all_messages');
+      let all_history = await messageHistory.json();
+      this.setState({msgs: all_history.payload});
+    }, 500);
+
+  }
+
 
   render() {
     return (
       <div>
-       <MsgInput />
+        <ChatHistory
+          messages={this.state.msgs}
+          />
+        <MsgInput
+          updater={(msg) =>
+            this.setState({msgs:this.state.msgs.concat([msg])})
+          }
+          />
       </div>
     );
   }
+
 };
 
 
