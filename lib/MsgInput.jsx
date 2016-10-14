1a1946a1c6e169c27d501343c5c5390c373defb5
edgarkhanzadyan <edgar.khanzadyan@yandex.ru>
2016-10-14 22:31:56 +0300

    'Initialization'

--- lib/MsgInput.jsx
+++ lib/MsgInput.jsx
@@ -1,91 +1,96 @@
 'use strict';
-
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {server_addr} from './globals';
-
 export default
 class MsgInput extends React.Component {
   constructor(){
     super();
     this.click_handler = this.click_handler.bind(this);
     this.form_changed = this.form_changed.bind(this);
-    this.state = {msg : ''};
-
+    this.form_changed2 = this.form_changed2.bind(this);
+    this.state = {msg : '', username: ''};
   }
-
    async click_handler(event) {
-    if(event.button == 0 || event.key == 'Enter') {
+    if((event.button == 0 || event.key == 'Enter') && this.state.msg !== '') {
       let req_opts = {
         method:'post',
         headers: new Headers({
           'Accept': 'application/json',
           'Content-Type': 'application/json'
         }),
-        body:JSON.stringify({'msg':this.state.msg})
+        body:JSON.stringify({'msg':this.state.username+': '+this.state.msg})
       };
       let request = server_addr + '/message'
       await fetch(request, req_opts);
       this.props.updater(this.state.msg);
-      this.setState({msg: ''});
+      this.setState({msg: '', username : this.state.username});
   }
-
+}
+  form_changed2(e) {
+    const userName = e.currentTarget.value;
+    this.setState({msg:this.state.msg, username:userName});
   }
-
-
-
   form_changed (e) {
     const letter = e.currentTarget.value;
-    this.setState({msg:letter});
+    this.setState({msg:letter, username: this.state.username});
   }
-
   render() {
     let sendMsgButton = {
       backgroundColor: '#4CAF50',
       border: 'none',
-      width: '100%',
+      width: '90%',
       height: '1.5rem',
       color: 'white',
       margin: '.25rem',
       justifyContent: 'center',
       borderRadius: '5px'
     };
-
     let msgContainer = {
       display: 'inline',
-      postition: 'fixed',
       flexDirection: 'column',
       width: '100%',
-      height: '100%',
       postition: 'fixed',
-      bottom: '0px',
-      left: '0px'
-
+      bottom: '0px'
     };
-
     let textInput = {
       display: 'flex',
       justifyContent: 'center',
-      width: '100%',
+      width: '90%',
       height: '1rem'
     };
-
-    let toolbarContainer = {
-      margin: '0px auto',
-      maxWidth: '980px'
-    }
-//
+    let setNameButton = {
+      backgroundColor: '#4CAF50',
+      border: 'none',
+      width: '50',
+      color: 'white',
+      borderRadius: '5px'
+    };
+    let td
     return (
-      <div style = {msgContainer}>
-        <div style = {toolbarContainer}>
-          <input type={"text"}
-                  style={textInput}
-                  onChange={this.form_changed}
-                  value={this.state.msg} onKeyDown = {this.click_handler} />
-          <button style = {sendMsgButton} onClick = {this.click_handler}>
-  	        Send
-          </button>
-        </div>
+      <div>
+        <table>
+          <tr>
+            <td>
+              <input type="text"
+                      onChange={this.form_changed2}
+                      value = {this.state.username}
+                      style={textInput}
+                      placeholder='Your Name' />
+            </td>
+            <td>
+            <input type="text"
+                    style={textInput}
+                    onChange={this.form_changed}
+                    value={this.state.msg}
+                    onKeyDown = {this.click_handler}
+                    placeholder='Message' />
+            </td>
+          </tr>
+        </table>
+        <button style = {sendMsgButton} onClick = {this.click_handler}>
+	        Send
+        </button>
       </div>
     );
   }
